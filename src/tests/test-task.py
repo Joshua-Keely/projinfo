c39c11f926475f69ab7f1e61f47d43b489495f37
keelyj <keelyj@etu.univ-grenoble-alpes.fr>
2023-09-26 11:59:21 +0200

    Partie 9

--- src/tests/test-task.py
+++ src/tests/test-task.py
@@ -1,6 +1,7 @@
-import src.tasks.task
+import tasks.task
 import pytest
-
+import datetime
+from mock import mock
 
 class TestTask:
     """
@@ -9,25 +10,25 @@ class TestTask:
 
     def test_getter_description(self):
         """Teste le getter de `__description` d'une tâche."""
-        tache = src.tasks.task.Task("test de description")
+        tache = tasks.task.Task("test de description")
         val = tache.description
         assert val == "test de description"
 
     def test_getter_state(self):
         """Teste le getter de '__state' d'une tache. """
-        tache = src.tasks.task.Task("test d'etat", True)
+        tache = tasks.task.Task("test d'etat", True)
         val = tache.state
         assert val is True
 
     def test_state_par_defaut(self):
         """Teste l'etat par defaut"""
-        tache = src.tasks.task.Task("test d'etat par defaut")
+        tache = tasks.task.Task("test d'etat par defaut")
         val = tache.state
         assert val == False
 
     def test_setter_description(self):
         """Teste le setter de `__description` d'une tâche."""
-        tache = src.tasks.task.Task("test de description")
+        tache = tasks.task.Task("test de description")
         tache.description = "nouvelle description"
         val = tache.description
         if tache.state == "à faire":
@@ -39,6 +40,19 @@ class TestTask:
                                  pytest.param(True, False, id="fait->a faire")
                              ])
     def test_change_state(self, etat_ini, etat_final):
-        tache = src.tasks.task.Task("Un descriptif", state=etat_ini)
+        tache = tasks.task.Task("Un descriptif", state=etat_ini)
         tache.change_state()
         assert tache.state == etat_final
+
+    # def test_date(self):
+    #     """Teste la date de création de la tâche."""
+    #     tache = src.tasks.task.Task("test de date")
+    #     val = tache.date
+    #     assert val == datetime.date.today()
+
+    @mock.patch('tasks.task.get_today',
+                return_value=datetime.date.fromisoformat("2023-08-23"))
+    def test_getter_date(self, mocker):
+        tache = tasks.task.Task("un descriptif")
+        assert tache.date == "23/08/2023"
+
